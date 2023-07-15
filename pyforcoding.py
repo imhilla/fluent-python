9b77ee54e0e77170956a39b66a755f053ef5c9c8
imhilla <hillaryodhiambo282@gmail.com>
2023-07-15 06:16:10 +0300

    add hash sets

--- pyforcoding.py
+++ pyforcoding.py
@@ -170,3 +170,29 @@ print(queue)
 
 queue.pop()
 print(queue)
+
+# hashset
+
+my_set = set()
+my_set.add(1)
+my_set.add(2)
+print(my_set)
+print(len(my_set))
+
+print(1 in my_set)
+print(2 in my_set)
+print(3 in my_set)
+
+my_set.remove(2)
+print(my_set)
+
+# list to set
+print(set([1, 2, 2, 3]))
+
+# set comprehension
+
+my_set = {i for i in range(5)}
+print(my_set)
+
+# hash maps
+my_map = {}
